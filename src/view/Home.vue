239716213597313a769eb79e50e4968817d69995
Javlonbek Tuychiyev <president7566@gmail.com>
2023-06-14 09:16:53 +0500

    fix

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,52 +1,10 @@
 <template>
     <div class="grid grid-flow-col auto-cols-max">
-        <n-card class="mr-6">
-            <n-tag type="success" class="justify-start"> Продажи </n-tag>
-        </n-card>
-        <n-card :segmented="{
-            content: true,
-            footer: 'soft'
-        }">
-            <template #header-extra>
-                #header-extra
-            </template>
-            Card Content
-            <template #footer>
-                #footer
-            </template>
-            <template #action>
-                #action
-            </template>
-        </n-card>
-        <n-card :segmented="{
-            content: true,
-            footer: 'soft'
-        }">
-            <template #header-extra>
-                #header-extra
-            </template>
-            Card Content
-            <template #footer>
-                #footer
-            </template>
-            <template #action>
-                #action
-            </template>
-        </n-card>
-        <n-card :segmented="{
-            content: true,
-            footer: 'soft'
-        }">
-            <template #header-extra>
-                #header-extra
-            </template>
-            Card Content
-            <template #footer>
-                #footer
-            </template>
-            <template #action>
-                #action
-            </template>
-        </n-card>
+        <div class="mr-6">
+            <n-card>
+                <n-tag type="success" class="justify-start"> Продажи </n-tag>
+                224
+            </n-card>
+        </div>
     </div>
 </template>
