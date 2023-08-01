77e7f6f567b7fc30633e3d7f43fa6f895ec18c70
Javlonbek Tuychiyev <president7566@gmail.com>
2023-08-02 00:50:13 +0500

    fix

--- src/view/new/Promocode.vue
+++ src/view/new/Promocode.vue
@@ -1,5 +1,32 @@
+<script setup>
+import axios from '../../api/axios'
+import Table from '../../components/Table.vue'
+
+const promocodes = ref({ data: [] })
+const details = reactive({
+    query: {
+        page: 1,
+        column: 'id',
+        sort: 'desc',
+        limit: 15
+    }
+})
+const getPromocodes = onMounted(async () => {
+    promocodes.value = (await axios.get('promocode/list', {
+        params: details.query
+    })).data
+})
+
+const pagination = (page) => {
+    details.query.page = page
+    getPromocodes()
+}
+
+const pageSize = (size) => {
+    details.query.limit = size
+    getPromocodes()
+}
+</script>
 <template>
-    <div>
-        New Promo page
-    </div>
+    <Table :data="promocodes.data" :column="details.columns" :pagination="pagination" :pageSize="pageSize" />
 </template>
