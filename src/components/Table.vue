42962764a1e4838d4422a14016e28157653047d8
Javlonbek Tuychiyev <president7566@gmail.com>
2023-08-18 10:11:31 +0500

    fix

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -8,16 +8,17 @@ const prop = defineProps({
     pageSize: Function,
     filters: Function,
     sort: Function,
+    rowKey: Function,
     class: String,
 })
 </script>
 <template>
     <div class="p-5 rounded" :class="useTheme().isDark ? 'bg-zinc-900' : 'bg-white'">
         <n-data-table size="small" @update:filters="filters" :bordered="true" :data="data.data" :columns="column"
-            :striped="true" :scroll-x="1000" :single-line="false" @update:sorter="sort" :class="class"
-            :max-height="'65vh'" />
+            :striped="true" :scroll-x="1000" :single-line="false" @update:sorter="sort" :class="class" :max-height="'65vh'"
+            :row-key="rowKey" />
 
-        <n-pagination @update:page="pagination" :page="data.current_page" @update:page-size="pageSize"
+        <n-pagination v-if="pagination" @update:page="pagination" :page="data.current_page" @update:page-size="pageSize"
             :page-count="data.last_page" show-size-picker :page-sizes="[15, 30, 70, 100, 150, 200]" class="mt-4" />
     </div>
 </template>
--- src/view/new/Category.vue
+++ src/view/new/Category.vue
@@ -1,5 +1,107 @@
+<script setup>
+import axios from '../../api/axios'
+import Table from '../../components/Table.vue'
+import { NButtonGroup, NButton, NPopconfirm } from 'naive-ui'
+import { Pencil, TrashOutline, Add } from '@vicons/ionicons5'
+import { useTheme } from '../../stores/theme'
+import moment from 'moment'
+
+const categories = ref({ data: [] })
+const details = reactive({
+    showModal: false,
+    columns: [
+        {
+            title: 'Категории',
+            key: 'name'
+        },
+        {
+            title: 'Дата создания',
+            key: 'created_at',
+            render(row) {
+                return moment(row.created_at).format("DD.MM.YYYY | HH:mm")
+            },
+        },
+        {
+            title: 'Статус',
+            key: 'active',
+            render(row) {
+                return row.active ? '✅ Активен' : '☑️ Неактивен'
+            }
+        },
+        {
+            title: 'Действия',
+            align: 'center',
+            width: 100,
+            render: (row) => {
+                return h(NButtonGroup, null, {
+                    default: () => [
+                        h(NButton, { onClick: () => show(row.id, 'update') }, { default: () => h(renderIcon(Pencil)) }),
+                        h(NPopconfirm, { negativeText: null, positiveText: 'Да', onPositiveClick: () => destroy(row) }, { trigger: () => h(NButton, null, { default: () => h(renderIcon(TrashOutline)) }), default: () => "Вы уверены, что хотите это удалить?" })
+                    ]
+                })
+            }
+        }
+    ]
+})
+
+const getCategories = onMounted(async () => {
+    categories.value = (await axios.get('category', {
+        params: details.query
+    })).data
+})
+
+const store = () => {
+    formRef.value?.validate(async (errors) => {
+        if (!errors) {
+            !formValue.value.active ? formValue.value.active = true : false
+            if (await axios.post('category', JSON.stringify(Object.assign({}, formValue.value)), { type: 'store' })) {
+                getCategories()
+                details.showModal = false
+            }
+        } else {
+            console.log(errors)
+        }
+    })
+}
+
+const show = async (id) => {
+    formValue.value = (await axios.get('category/show/' + id)).data.data
+    formValue.value.role = formValue.value.role.id
+    formValue.value.active = formValue.value.active ? true : false
+    details.showModal = true
+}
+
+const update = async () => {
+    formRef.value?.validate(async (errors) => {
+        if (!errors) {
+            if (await axios.put('category/' + formValue.value.id, JSON.stringify(Object.assign({}, formValue.value)), { type: 'update' })) {
+                getCategories()
+                details.showModal = false
+            }
+        } else {
+            console.log(errors)
+        }
+    })
+}
+
+const destroy = async (row) => {
+    if (await axios.get('category/delete/' + row.id, { type: 'destroy' })) {
+        getCategories()
+        details.showModal = false
+    }
+}
+
+const rowKey = (row) => row.id
+</script>
+
 <template>
-    <div>
-        New category page
+    <div class="flex justify-end mb-3 rounded py-2 px-5" :class="useTheme().isDark ? 'bg-zinc-900' : 'bg-white'">
+        <n-button type="success" ghost @click="details.showModal = true; formValue = []">
+            <template #icon>
+                <Add />
+            </template>
+        </n-button>
     </div>
+
+    <Table :data="categories" :column="details.columns" :row-key="rowKey" />
 </template>
